7889f9cb613fce3dc0ac81b6a1344140d7d45e0a
anvaka <anvaka@gmail.com>
2015-11-05 21:34:50 -0800

    Render side by side

--- demo/index.js
+++ demo/index.js
@@ -4,56 +4,56 @@ var graph = require('./createGraph.js');
 
 var kruskal = require('../index.js');
 var path = kruskal(graph);
-
 // The rest is just trendering.
-path.forEach(indexLink);
+var pathGraph = Viva.Graph.graph();
+path.forEach(addToPathgraph);
+renderGraph(graph, 'left');
+renderGraph(pathGraph, 'right');
 
-var layout = Viva.Graph.Layout.forceDirected(graph, {
-  springLength: 35,
-  springCoeff: 0.00055,
-  dragCoeff: 0.09,
-  gravity: -1
-});
+function renderGraph(graph, container) {
+  var layout = Viva.Graph.Layout.forceDirected(graph, {
+    springLength: 35,
+    springCoeff: 0.00055,
+    dragCoeff: 0.09,
+    gravity: -1
+  });
 
-var svgGraphics = Viva.Graph.View.svgGraphics();
-svgGraphics.node(function(node) {
-  var groupId = node.data.group;
-  var color = '#dedede';
-  var circle = Viva.Graph.svg('circle')
-    .attr('r', 7)
-    .attr('stroke', '#fff')
-    .attr('stroke-width', '1.5px')
-    .attr("fill", color);
+  var svgGraphics = Viva.Graph.View.svgGraphics();
+  svgGraphics.node(function(node) {
+    var color = '#dedede';
+    var circle = Viva.Graph.svg('circle')
+      .attr('r', 7)
+      .attr('stroke', '#fff')
+      .attr('stroke-width', '1.5px')
+      .attr("fill", color);
 
-  circle.append('title').text(node.data.name);
+    return circle;
+  }).placeNode(function(nodeUI, pos) {
+    nodeUI.attr("cx", pos.x).attr("cy", pos.y);
+  });
 
-  return circle;
-}).placeNode(function(nodeUI, pos) {
-  nodeUI.attr("cx", pos.x).attr("cy", pos.y);
-});
+  svgGraphics.link(function(link) {
+    var width = 1;
+    var color = '#999';
+    if (pathGraph.getLink(link.fromId, link.toId)) {
+      width = 3;
+      color = '#ff00af';
+    }
+    return Viva.Graph.svg('line')
+      .attr('stroke', color)
+      .attr('stroke-width', width);
+  });
 
-svgGraphics.link(function(link) {
-  var width = 1;
-  var color = '#999';
-  if (link.onPath) {
-    width = 3;
-    color = '#ff00af';
-  }
-  return Viva.Graph.svg('line')
-    .attr('stroke', color)
-    .attr('stroke-width', width);
-});
+  var renderer = Viva.Graph.View.renderer(graph, {
+    container: document.getElementById(container),
+    layout: layout,
+    graphics: svgGraphics,
+    prerender: 20
+  });
 
-var renderer = Viva.Graph.View.renderer(graph, {
-  container: document.getElementById('graph'),
-  layout: layout,
-  graphics: svgGraphics,
-  prerender: 20
-});
+  renderer.run();
+}
 
-renderer.run();
-
-function indexLink(el) {
-  var link = graph.getLink(el.fromId, el.toId);
-  link.onPath = true;
+function addToPathgraph(el) {
+  pathGraph.addLink(el.fromId, el.toId);
 }
